0212fb5a9abb05ed9d60551bb4776a68b1271f15
GLOBAL\Andrew.Mole <andrew.mole@arup.com>
2021-10-07 10:34:08 +0800

    Developed loop funcs for identifying beam perims

--- eng_utilities/polyline_utilities.py
+++ eng_utilities/polyline_utilities.py
@@ -1,6 +1,8 @@
 """"""
 
 from eng_utilities.geometry_utilities import *
+from collections import namedtuple, OrderedDict
+from operator import le, lt
 
 def bounding_box(coords):
     """Runs through a collection of x,y tuple pairs and 
@@ -171,7 +173,7 @@ def loop_finder(pt_dict, connections_dict, start_pt_ID=None):
     check for crossing lines.
 
     Args:
-        coords_dict:  is a collection of 'pt_ID: (x, y, z)'
+        pt_dict:  is a collection of 'pt_ID: (x, y, z)'
             key-value pairs
         connections_dict: is a collection of 
             'pt0_ID: [pt1_ID, pt2_ID, pt3_ID, ...]'
@@ -179,36 +181,65 @@ def loop_finder(pt_dict, connections_dict, start_pt_ID=None):
             referenced points pt1_ID, pt2_ID etc.
         start_pt_ID: """
     
-    if start_pt_ID is None:
+    if start_pt_ID is not None:
         pt0_ID = start_pt_ID
     else:
         pt0_ID = min_x_pt(pt_dict)
-        polar_pt0 = cart2cyl(pt_dict.get(pt0_ID))
+        
     theta = 0
     
-    loop_list = [pt0_ID]
+    loop_ID_list = [pt0_ID]
+    old_ID = None
     pt_ID = pt0_ID
     while True:
         connected_node_IDs = connections_dict.get(pt_ID, [])
-        if len(connected_node_IDs) == 0 or connected_node_IDs == [pt0_ID]:
-            loop_list = [pt_ID]
+        node_IDs = [node_ID for node_ID in connected_node_IDs if node_ID != old_ID]
+        pt_coords = pt_dict.get(pt_ID)
+        if len(node_IDs) == 0 and pt_ID == pt0_ID:
+            loop_ID_list = [pt0_ID]
             break
+        
+        if len(node_IDs) == 0 and old_ID in connected_node_IDs:
+            new_pt_ID = old_ID # to allow it to return from a branch
+            theta = angfix(theta + pi) # 180deg reversed
         else:
-            node_coords = [pt_dict.get(node_ID) for node_ID in connected_node_IDs]
-            polar_coords = [cart2cyl(coords) for coords in node_coords]
-            angles = [angfix(ang - theta) for _, ang, _ in polar_coords]
-            min_ang_pt = min((ang, ID) for ang, ID in zip(angles, connected_node_IDs))
+            # note that this does not currently handle situations where beams have zero length
+            node_coords = [subND(pt_dict.get(node_ID), pt_coords) for node_ID in node_IDs] # relative vectors
+            # convert to polar coordinates (assigning high angle if magnitude is zero)
+            polar_coords = [cart2cyl(coords, 9 * pi) for coords in node_coords] # polar coordinates
+            angles = [ang for _, ang, _ in polar_coords] # extract angles
+            # convert to relative angles (assigning high angle to case already assigned high angle)
+            rel_angles = [(angfix(ang - theta) if ang <= pi else (9 * pi)) for ang in angles] # angles relative to the incoming beam
+            # choose pt with minimum relative angle (i.e. on the right side for anti-clockwise loop)
+            min_ang_pt = min((rel_ang, ID, ang) for rel_ang, ID, ang in zip(rel_angles, node_IDs, angles))
             new_pt_ID = min_ang_pt[1]
-            if len(loop_list) > 1:
-                if new_pt_ID == loop_list[1]:
+            theta = min_ang_pt[2]
+            if len(loop_ID_list) > 1:
+                if new_pt_ID == loop_ID_list[1]:
                     break
-            loop_list.append(new_pt_ID)
-            pt_ID = new_pt_ID
-    return loop_list
+        loop_ID_list.append(new_pt_ID)
+        old_ID = pt_ID
+        pt_ID = new_pt_ID
+    return loop_ID_list
 
 
-def line_intersection2D(line1, line2):
-    """Lines are defined as a tuple of tuples:
+def line_intersection2D(line1, line2, is_inclusive=True):
+    """Returns a dictionary of any intersection when 
+    provided with two lines (each defined as a pair of tuples).
+
+    The intersection type is reported as:
+        'parallel':  lines are parallel (no intersection)
+        'neither' :  intersection is outside both lines
+        'line1'   : intersection is only inside the extent of line1
+        'line2'   :  intersection is only inside the extent of line2
+        'both'    : intersection occurs within both line extents
+
+    Intersections are defined in the XY plane. Other coordinates
+    will be carried over, but ignored. If the is_inclusive option
+    is maintained, crossing definition will include direct 
+    intersections at line ends.
+
+    Lines are defined as a tuple of tuples:
         ((x1, y1), (x2, y2))
     These are then converted into parametric format:
         point + t * vector
@@ -219,45 +250,152 @@ def line_intersection2D(line1, line2):
     Result: 
 
     """
-    p1, v1 = line1[0], sub2D(line1[1], line1[0])
-    p2, v2 = line2[0], sub2D(line2[1], line2[0])
+    op = le if is_inclusive else lt
+
+    p1, v1 = line1[0], subND(line1[1], line1[0])
+    p2, v2 = line2[0], subND(line2[1], line2[0])
      
     denom = v1[0] * v2[1] - v1[1] * v2[0]
     if denom == 0:
         return {'type': 'parallel'}
     else:
         crossing_type = None
-        t1 = ((p1[1] - p2[1]) * v2[0] - (p1[0] - p2[0]) * v2[1]) / denom
-        if (0 <= t1 <= 1):
+        t1 = ((p1[1] - p2[1]) * v2[0] - (p1[0] - p2[0]) * v2[1]) / denom        
+        if op(0, t1) and op(t1, 1):
             crossing_type = 'line1'
         t2 = ((p1[1] - p2[1]) * v1[0] - (p1[0] - p2[0]) * v1[1]) / denom
-        if (0 <= t2 <= 1):
+        if op(0, t2) and op(t2, 1):
             crossing_type = 'both' if crossing_type else 'line2'
         if crossing_type is None:
             crossing_type = 'neither'
-        return {'intersection': add2D(p1, scale2D(v1, t1)), 
+        return {'intersection': addND(p1, scaleND(v1, t1)), 
         't1': t1, 't2': t2, 'type': crossing_type,
         }
     
 
-def self_intersections(line_list, sorted=False):
-    if sorted == False:
+def self_intersections(line_list, is_sorted=False, is_inclusive=False):
+    """Calculates self intersections using a sweep algorithm
+    along the x-axis.
+    
+    line_list is a list of lines defined as pairs of tuples"""
+    if is_sorted == False:
         sorted_list = sorted((pt1, pt2) if pt1[0] < pt2[0] else (pt2, pt1) 
             for pt1, pt2 in line_list)
     else:
-        sorted_list = line_list
+        sorted_list = list(line_list)
     
     line_stack = []
     cross_list = []
 
     for line in sorted_list:
         x = line[0][0]
-        line_stack2 = []
+        next_stack = []
         for line2 in line_stack:
-            if line2[0][0] >= x:
-                line_stack2.append(line2)
-                crossing = line_intersection2D(line, line2)
+            if line2[1][0] >= x:
+                next_stack.append(line2)
+                crossing = line_intersection2D(line, line2, is_inclusive)
                 if crossing['type'] == 'both':
                     cross_list.append(crossing['intersection'])
-        line_stack = line_stack2.copy()
+        line_stack = next_stack.copy()
         line_stack.append(line)
+    return cross_list
+
+
+def line_interpolate_y(line, x):
+    """Interpolates y values on a line when given x
+    To avoid duplicate hits for nodes level with the 
+    joints between lines, the start of a line is not 
+    considered an intersection."""
+    #if line[0][0] == x:
+    #    return line[0][1]
+    if line[1][0] == x:
+        return line[1][1]
+    elif (line[0][0] < x < line[1][0]) and ((line[1][0] - line[0][0]) != 0):
+        return line[0][1] + (x - line[0][0]) * (line[1][1] - line[0][1]) / (line[1][0] - line[0][0])
+    else:
+        return None
+
+
+def nodes_outside_loop(node_coord_dict, pt_loop):
+    """Returns a list of nodes outside a loop
+    Note that the loop must be closed"""
+    #coord_loop = [node_coord_dict.get(pt) for pt in pt_loop]
+    line_loop = [(node_coord_dict.get(pt1), node_coord_dict.get(pt2)) 
+        for pt1, pt2 in zip(pt_loop[:-1], pt_loop[1:])]
+    
+    # Sort the lines and points based on the x-axis
+    sorted_line_list = sorted((pt1, pt2) if pt1[0] < pt2[0] else (pt2, pt1) 
+            for pt1, pt2 in line_loop)
+    
+    sorted_point_list = sorted((coords[0], coords[1], ID) 
+            for ID, coords in node_coord_dict.items())
+    
+    line_gen = (line for line in sorted_line_list)
+    #point_gen = (point for point in sorted_point_list)
+
+    line_stack = []
+    outside_nodes = []
+    # iterate over the nodes in the list
+    for node in sorted_point_list:
+        if node[2] in pt_loop:  # ignore nodes in loop 
+            continue
+        x_node, y_node, nodeID = node
+        # trim stack
+        line_stack = [line for line in line_stack if line[1][0] > x_node]
+        
+        while True:
+            try:
+                line = next(line_gen)
+            except StopIteration:
+                break
+            # check whether line still covers node 
+            if line[0][0] < x_node <= line[1][0]:
+                line_stack.append(line)
+            elif line[0][0] > x_node:
+                break
+        
+        # TODO
+        if len(line_stack) == 0:
+            break
+        # check node against limits - how many crossings
+        y_values = [line_interpolate_y(line, x_node) for line in line_stack]
+        if sum(y for y in y_values if y >= y_node if y is not None) % 2 == 0:
+            outside_nodes.append(nodeID)
+        
+    return outside_nodes
+        
+    
+def all_loops_finder(pt_dict, connections_dict, sort_points=True):
+    """Returns a list containing 
+    a closed loop of connected points"""
+
+    first_point_ID = None
+
+    # Sort the pt_dict according to x
+    if sort_points:
+        pt_dict = {k:v for v, k in sorted([(v, k) for k, v in pt_dict.items()])}
+        
+
+    loops_list = []
+
+    while True:
+        if sort_points:
+            first_point_ID = list(pt_dict)[0]
+        loop = loop_finder(pt_dict, connections_dict, first_point_ID)
+        if len(loop) == 1:
+            pt_dict.pop(loop[0])
+            if len(pt_dict) == 0:
+                break
+        elif len(loop) > 1:
+            outside_nodes = nodes_outside_loop(pt_dict, loop)
+            loops_list.append(loop)
+            if len(outside_nodes) > 0:                
+                pt_dict = {k:v for k, v in pt_dict.items() if k in outside_nodes}
+            else:
+                break
+        else:  # something has gone wrong
+            if len(pt_dict) > 0:
+                print(f'Something has gone wrong in loop finding...')
+                print(f'{len(pt_dict)} nodes are still in pt_dict')
+            break
+    return loops_list
